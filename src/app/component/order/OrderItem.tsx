b518b93ce7b2d902d6686c7c0af27875b5ca8bd0
ashmna <mnatsakanyan.ashot@gmail.com>
2017-04-15 19:37:20 +0400

    Hackathon

--- src/app/component/order/OrderItem.tsx
+++ src/app/component/order/OrderItem.tsx
@@ -1,25 +1,34 @@
 import * as React from "react";
+import {CourierSelect} from  "../order/CourierSelect"
 import {colors} from "material-ui/styles";
 import Paper from "material-ui/Paper";
-import {CourierSelect} from  "../order/CourierSelect"
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import OrderLocationIcon from "material-ui/svg-icons/communication/location-on";
+import OrderFoodsIcon from "material-ui/svg-icons/action/home";
+import RestaurantLocationIcon from "material-ui/svg-icons/action/shopping-cart";
+import BicycleIcon from "material-ui/svg-icons/maps/directions-bike";
+import CarIcon from "material-ui/svg-icons/maps/directions-boat";
+import BikeIcon from "material-ui/svg-icons/action/motorcycle";
 
 export interface OrderItemProps {
     orderData: any;
 }
 
 export interface OrderItemState {
-    active: boolean;
+    active?: boolean;
+    selectedCourier?: any;
 }
 
 
 const style = {
     normal: {
         margin: "5px",
-        padding: "0 10px",
+        padding: "10px",
     },
     active: {
-        margin: "5px 5px 5px 10px",
-        padding: "0 10px",
+        margin: "5px",
+        padding: "10px",
     },
     normalIcon: {
         color: colors.grey200,
@@ -31,15 +40,26 @@ const style = {
 
 export class OrderItem extends React.Component<OrderItemProps, OrderItemState> {
 
+
+    private couriers: any = [
+        {id: 1, transport_type: "Car", name: "Vle"},
+        {id: 2, transport_type: "Bicycle", name: "Gugo"},
+        {id: 3, transport_type: "Bike", name: "Xcho"},
+    ];
     constructor(props: OrderItemProps, context: any) {
         super(props, context);
         this.state = {
             active: false,
+            selectedCourier: null,
         };
     }
 
     private send(){
         console.log(this.props.orderData)
+    }
+
+    private courierChangeHandler(event: any, index: number, value: any) {
+        this.setState({selectedCourier: value});
     }
 
     render() {
@@ -50,15 +70,69 @@ export class OrderItem extends React.Component<OrderItemProps, OrderItemState> {
                  onMouseEnter={() => this.setState({active: true})}
                  onMouseLeave={() => this.setState({active: false})}
             >
-                <div className="row">
-                    <div className="col-md-3">{this.props.orderData.id}</div>
-                    <div className="col-md-3">{this.props.orderData.address}</div>
-                    <div className="col-md-3">{this.props.orderData.restourant}</div>
-                    <div className="col-md-3">{this.props.orderData.courier}</div>
-                    <CourierSelect couriers={this.props.orderData.couriers}/>
-                    <button onClick={this.send.bind(this)}>Send</button>
-                </div>
+                <div className="col-md-8">
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <OrderLocationIcon color={colors.grey600}/>
+                            {this.props.orderData["order_address"]}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <OrderFoodsIcon color={colors.grey600}/>
+                            {this.props.orderData["order_item_info"]}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <RestaurantLocationIcon color={colors.grey600}/>
+                            {this.props.orderData["restaurant_info"]}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <SelectField
+                                value={this.state.selectedCourier}
+                                onChange={this.courierChangeHandler.bind(this)}
+                                fullWidth={true}
+                            >
+                                {this.couriers.map((courier: any, index: number) => (
+                                    <MenuItem
+                                        value={courier}
+                                        key={index}
+                                        primaryText={courier.name}
+                                        secondaryText={courier.transport_type}
+                                    />
+                                ))}
 
+                            </SelectField>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="row">
+                        <div className="col-xs-12">
+                            {}
+
+                            {this.props.orderData["order_address"]}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            {this.state.selectedCourier["transport_type"] === "" && <OrderLocationIcon color={colors.grey600}/>}
+                            <h2>{this.state.selectedCourier["name"]}</h2>
+                            directions bike
+                            directions car
+                            motorcycle
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <RestaurantLocationIcon/>
+                            {this.props.orderData["restaurant_info"]}
+                        </div>
+                    </div>
+                </div>
             </Paper>
         );
     }
--- src/app/service/OrderService.ts
+++ src/app/service/OrderService.ts
@@ -7,16 +7,21 @@ export class OrderService {
         console.log('order service');
         return new Promise((resolve, reject) => {
             resolve([
-                {id:45,address:'ave',restaurant:{address:'1 str.',name:'krs'},couriers:{selected:10,list:[10,20,30]}},
+                {
+                    id: 45,
+                    order_address: "36/2 Pushkin St, Yerevan, Armenia",
+                    order_item_info: "4 մեծ շաուրմա, 2 կոլա, 3 թան",
+                    restaurant_info: "Tumanyan Shaurma | 32 Tumanyan St, Yerevan 0001, Armenia",
+                },
             ]);
             /*ajax({
-                method: "GET",
-                url: url("order"),
-                type: "jsonp",
-                contentType: "application/json; charset=utf-8",
-            })
-                .done(resolve)
-                .fail(reject);*/
+             method: "GET",
+             url: url("order"),
+             type: "jsonp",
+             contentType: "application/json; charset=utf-8",
+             })
+             .done(resolve)
+             .fail(reject);*/
         });
     }
 }
